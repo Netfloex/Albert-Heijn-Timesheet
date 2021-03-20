47f76aa707eaf2f55052fae04dc5a90b7122b3e9
Netfloex <netfloex@gmail.com>
2021-03-20 02:21:55 +0100

    Use lowdb for caching the token

--- bin/SamLogin.ts
+++ bin/SamLogin.ts
@@ -1,14 +1,31 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
-import { stringify } from "querystring";
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from "axios";
+import FileAsync from "lowdb/adapters/FileAsync";
+import low from "lowdb";
+
+type Schema = {
+	token: string;
+	expiry: number;
+};
+
 export default class SamLogin {
+	private db: low.LowdbAsync<Schema>;
 	private http: AxiosInstance;
 
 	private username: string;
 	private password: string;
 
-	private session: string;
-	private token: string;
-
+	get token() {
+		return this.db.get("token").value();
+	}
+	get expiry() {
+		return this.db.get("expiry").value();
+	}
+	set token(value) {
+		this.db
+			.set("token", value)
+			.set("expiry", Date.now() + 60000)
+			.write();
+	}
 	constructor({ username, password }: { username: string; password: string }) {
 		this.http = axios.create({
 			baseURL: "https://sam.ahold.com/",
@@ -16,40 +33,47 @@ export default class SamLogin {
 		this.username = username;
 		this.password = password;
 	}
-
+	async init() {
+		this.db = await low(new FileAsync<Schema>("store.json"));
+	}
 	async login() {
-		this.session = await this.getSession();
-		this.token = await this.loginRequest();
+		await this.db.read();
+		if (Date.now() > this.expiry) {
+			console.log("Token is expired");
+			var session = await this.getSession();
+			var token = await this.loginRequest(session).catch((err: AxiosError) => {
+				if (err?.response?.status === 200) {
+					console.log(`Wachtwoord Fout`.toUpperCase());
+				} else {
+					throw err;
+				}
+			});
+			if (token) {
+				this.token = token;
+			}
+		}
 		return this.token;
+	}
+
+	private firstCookie(headers: AxiosResponse["headers"]) {
+		return headers["set-cookie"][0].split(";")[0] as string;
 	}
 
 	private async getSession() {
 		var res = await this.http("wrkbrn_jct/etm/time/timesheet/etmTnsMonth.jsp");
-		var cookies = res.headers["set-cookie"];
-		return cookies[0].split(";")[0];
+		return this.firstCookie(res.headers);
 	}
 
-	private async loginRequest() {
-		var res: AxiosResponse;
-		try {
-			var res = await this.http.post(
-				"pkmslogin.form",
-				stringify({
-					username: this.username,
-					password: this.password,
-					"login-form-type": "pwd",
-				}),
-				{
-					headers: { Cookie: this.session },
-					maxRedirects: 0,
-					validateStatus: s => s == 302,
-				}
-			);
-		} catch (error) {
-			console.log(error.response);
-
-			return "Awww";
-		}
-		return res.headers["set-cookie"][0].split(";")[0];
+	private async loginRequest(session: string) {
+		var res = await this.http.post(
+			"pkmslogin.form", //
+			`username=${this.username}&password=${this.password}&login-form-type=pwd`,
+			{
+				headers: { Cookie: session },
+				maxRedirects: 0,
+				validateStatus: s => s == 302,
+			}
+		);
+		return this.firstCookie(res.headers);
 	}
 }
--- index.ts
+++ index.ts
@@ -3,10 +3,17 @@ console.log("Started");
 import express from "express";
 import SamLogin from "./bin/SamLogin";
 import config from "./config.json";
+
 const Sam = new SamLogin(config);
 const app = express();
+console.time("init");
+Sam.init().then(() => {
+	console.timeEnd("init");
 
-app.get("/appie", async (_, res) => {
-	res.json(await Sam.login());
+	app.get("/appie", async (_, res) => {
+		console.time("Appie");
+		res.json(await Sam.login());
+		console.timeEnd("Appie");
+	});
+	app.listen(4444);
 });
-app.listen(4444);
